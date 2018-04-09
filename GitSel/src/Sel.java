1dcfcd11d54b8bf95ed48b53c018161fcf5ddc9a
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-04-09 16:30:55 +0530

    Update Sel.java

--- GitSel/src/Sel.java
+++ GitSel/src/Sel.java
@@ -3,6 +3,7 @@ public class Sel {
 
 	public static void main(String[] args) {
 		System.out.println("Exception while taking screenshot ");
+		System.out.println("Exception while taking screenshot 111 ");
 
 	}
 
